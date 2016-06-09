d0ad14bda90e6450f6b37f53ff6305f5295c8f3b
Eva <Bresciano>
2016-06-08 20:06:23 -0600

    Finished

--- FindMyRep/FindMyRep/RepController.swift
+++ FindMyRep/FindMyRep/RepController.swift
@@ -10,33 +10,37 @@ import Foundation
 
 class RepController {
     
-    var reps: [Rep] = []
-    
     static let sharedInstance = RepController()
     
-    static let baseURLString =  "http://whoismyrepresentative.com/getall_reps_bystate.php?state"
+    static let baseURLString =  "http://whoismyrepresentative.com/getall_reps_bystate.php"
+    
+//    static func searchURLByState(state: String) -> NSURL {
+//        return NSURL(string: baseURLString + "/getall_reps_bystate.php?state=\(state)&output=json")!
+//    }
     
     static func searchRepsByState(state: String, completion: (reps : [Rep]) -> Void) {
         
-        let url = searchURLByState(state)
+        guard let url = NSURL(string: baseURLString) else {
+            completion(reps: [])
+            return
+        }
         
-        NetworkController.performRequestForURL(url, httpMethod: .Get) { (data, error) in
+        let urlParameters = ["state":"\(state)", "output": "json"]
+        
+        NetworkController.performRequestForURL(url, httpMethod: .Get, urlParameters: urlParameters) { (data, error) in
             guard let data = data,
                 let jsonDictionary = (try? NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as? [String:AnyObject]),
                 let repDictionaries = jsonDictionary?["results"] as? [[String: AnyObject]] else {
                     
-                    print("Unable to serialize json")
+                    print("could not serialize json")
                     completion(reps: [])
                     return
                 }
             
             let reps = repDictionaries.flatMap({Rep(jsonDictionary:$0)})
+            completion(reps: reps)
         }
         
-    }
-    
-    static func searchURLByState(state: String) -> NSURL {
-        return NSURL(string: baseURLString + "/getall_reps_bystate.php?state=\(state)&output=json")!
     }
     
 }
